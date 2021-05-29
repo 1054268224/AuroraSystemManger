19ef9b894157e81eeb1ac5368ce8383f57d18091
shuai.hu <shuai.hu@wheatek.com>
2021-05-29 12:08:09 +0800

    x

--- SystemManager/ui/src/main/java/com/example/systemmanageruidemo/actionpresent/OptimisePresent.java
+++
@@ -1,6 +0,0 @@
-package com.example.systemmanageruidemo.actionpresent;
-
-import com.example.systemmanageruidemo.actionview.OptimiseView;
-
-public interface OptimisePresent  extends PresentI<OptimiseView>{
-}
--- SystemManager/ui/src/main/java/com/example/systemmanageruidemo/actionview/OptimiseView.java
+++
@@ -1,6 +0,0 @@
-package com.example.systemmanageruidemo.actionview;
-
-import com.example.systemmanageruidemo.actionpresent.OptimisePresent;
-
-public interface OptimiseView extends ViewAction<OptimisePresent> {
-}
